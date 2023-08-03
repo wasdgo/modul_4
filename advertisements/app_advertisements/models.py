2f07e9a68c92265f9ad17fa678f6c613450ad6cb
wasdgo <Timofey2008artem2@mail.ru>
2023-08-03 20:51:41 +0300

    ver 5

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Advertisement(models.Model):
+    title = models.CharField(verbose_name='заголовок', max_length=128)
+    description = models.TextField('описание')
+    price = models.DecimalField('цена', max_digits=10, decimal_places=2)
+    auction = models.BooleanField('торг', help_text='отметьте, если торг уместен')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+
--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -8,3 +8,8 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
+
